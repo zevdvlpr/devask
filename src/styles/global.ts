5991392cd952fa03a2af471ee847ba4320445686
zevdvlpr <zevdvlpr@gmail.com>
2021-03-22 19:51:09 -0300

    💡 chore: global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    
+    box-sizing: boder-box;
+  }
+  
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+    
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  } 
+  
+  body {              
+    background: ${({ theme }) => theme.purple};
+    
+    -webkit-font-smoothing: antialiased;
+  }
+        
+  body, input, textarea, button {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 400;
+  }
+  
+  h1, h2, h3, h4, h5, h5, strong {
+    font-weight: 700;
+  }
+  
+  button {
+    cursor: pointer;
+  }
+  
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  
+  [disabled] {
+    opacity: 0.6;
+    
+    cursor: not-allowed;
+  }
+`;
