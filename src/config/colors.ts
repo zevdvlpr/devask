a112e4fb745e6ef85975bb70e6fdb28fdbea7cbc
zevdvlpr <zevdvlpr@gmail.com>
2021-03-22 21:02:56 -0300

    💡 chore: new colors

--- src/config/colors.ts
+++ src/config/colors.ts
@@ -1,7 +1,11 @@
 export const colors = {
+  header: '#1B1F23',
+  input: '#121214',
+
+  background: '#202A32',  
+
   black: '#000',
   white: '#FFF',
-  purple: '#6F3BFF',
 };
 
 export function getColors() {
